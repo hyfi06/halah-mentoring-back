f538af7e27fe8e39f50788f1f538571d65b7d9eb
Alexis Araujo <ala1289@gmail.com>
2020-06-06 16:32:23 -0500

    Agregar codigo base servidor

---
+++ config/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const config = {
+  dev: process.env.NODE_ENV !== 'production',
+  port: process.env.PORT || 3000,
+  cors: process.env.CORS,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbName: process.env.DB_NAME,
+  authJwtSecret: process.env.AUTH_JWT_SECRET,
+  publicApiKeyToken: process.env.PUBLIC_API_KEY_TOKEN,
+  adminApiKeyToken: process.env.ADMIN_API_KEY_TOKEN,
+  dbCollections: {
+    users: 'usersCSV',
+    orders: 'orders',
+    apiKeys: 'apiKeys',
+    exams: 'exams',
+    results: 'results',
+  },
+};
+
+module.exports = { config };
